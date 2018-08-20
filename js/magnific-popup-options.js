ea611f218d2d0cba0675ef049f2bda91a029af8e
patricksanang <patrick.sanang@gmail.com>
2018-08-20 02:03:57 +0100

    some changes

--- js/magnific-popup-options.js
+++ js/magnific-popup-options.js
@@ -28,7 +28,27 @@ $(document).ready(function() {
 
 	var linkMagnifPopup = function() {
 		$('.image-popup-link').magnificPopup({
-		  type: 'image'
+		  type: 'image',
+			image: {
+			  markup: '<div class="mfp-figure">'+
+			            '<div class="mfp-close"></div>'+
+			            '<div class="mfp-img"></div>'+
+			            '<div class="mfp-bottom-bar">'+
+			              '<div class="mfp-title"></div>'+
+			              '<div class="mfp-counter"></div>'+
+			            '</div>'+
+			          '</div>', // Popup HTML markup. `.mfp-img` div will be replaced with img tag, `.mfp-close` by close button
+
+			  cursor: 'mfp-zoom-out-cur', // Class that adds zoom cursor, will be added to body. Set to null to disable zoom out cursor.
+
+			  titleSrc: function(item) {
+			     return item.el.attr('title') + '<small>' + item.el.attr('content') + '</small>' + '<small><a style="color: gray" href="'+item.el.attr('link')+'">' + item.el.attr('text') + '</a></small>';
+			  },
+
+			  verticalFit: true, // Fits image in area vertically
+
+			  tError: '<a href="%url%">The image</a> could not be loaded.' // Error message
+			}
 		});
 	};
 
@@ -44,10 +64,10 @@ $(document).ready(function() {
     });
 	};
 
-	
 
 
-	// Call the functions 
+
+	// Call the functions
 	magnifPopup();
 	linkMagnifPopup();
 	magnifVideo();
